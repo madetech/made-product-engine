b1b619e13d52a352b49d559ca2b7b230d24e329b
Seb Ashton <seb@madebymade.co.uk>
2013-03-27 11:30:40 +0000

    adds view on site

--- app/admin/category.rb
+++ app/admin/category.rb
@@ -6,6 +6,10 @@ if defined?(ActiveAdmin) and Product.config.engine_active_admin
 
     menu :label => 'Categories', :parent => "Products", :priority => 2
 
+    action_item :only => :show do
+      link_to "View on site", product_category_path(product_category.url)
+    end
+
     controller do
       cache_sweeper  Product.config.product_category_sweeper if Product.config.product_category_sweeper
       defaults :finder => :find_by_url
--- app/admin/item.rb
+++ app/admin/item.rb
@@ -7,6 +7,10 @@ if defined?(ActiveAdmin) and Product.config.engine_active_admin
 
     menu :label => 'Individual Product', :parent => "Products", :priority => 1
 
+    action_item :only => :show do
+      link_to "View on site", product_item.get_canonical_url
+    end
+
     controller do
       cache_sweeper  Product.config.product_item_sweeper if Product.config.product_item_sweeper
       defaults :finder => :find_by_url
